50a5c3125d17401ff5d171121cdd989f2161764d
Dicskson Owusu Nyantakyi <111504596+ondickson@users.noreply.github.com>
2025-07-11 04:56:00 +0800

    Update axios.js

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,10 +1,20 @@
 // frontend/src/api/axios.js
 import axios from 'axios';
 
-const baseURL =
-  window.location.hostname === 'localhost'
-    ? 'https://fliq-backend.onrender.com'
-    : 'http://172.18.80.1:5000';
+// const baseURL =
+//   window.location.hostname === 'localhost'
+//     ? 'http://localhost:5000'
+//     ? 'https://fliq-backend.onrender.com' 
+//     : 'http://172.18.80.1:5000';
+
+// Default to Render backend
+let baseURL = 'https://fliq-backend.onrender.com';
+
+// Use local backend during development
+if (window.location.hostname === 'localhost') {
+  baseURL = 'http://localhost:5000';
+}
+
 
 const instance = axios.create({
   baseURL,
